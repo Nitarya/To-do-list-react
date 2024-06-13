c47b2f9cee39a428492ae31d68512cfb34621072
Nitya <niittss27@gmail.com>
2024-06-13 15:23:25 +0530

    to-do-list

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='main_div'>
+      <div className='center_div'>
+    <br/>
+    <h1></h1>
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
