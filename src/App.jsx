a54c58f579496f80b2197a6e0c1493c3583067b3
Nitya <niittss27@gmail.com>
2024-06-13 16:19:11 +0530

    Placeholder and list names equivalent

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
-
-import './App.css'
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
- 
+
+const [inputList, setInputList] = useState("buy apple")
+
+const itemEvent = (event) => {
+setInputList(event.target.value)
+}
+
 
   return (
     <>
-    <div className='main_div'>
-      <div className='center_div'>
-    <br/>
-    <h1>ToDo List</h1>
-    <br />
-    <input type="text" placeholder="Add a Items"/>
-      </div>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input type="text" placeholder="Add a Items" onChange={itemEvent} />
+          <button> + </button>
 
-    </div>
+          <ol>
+            <li> {inputList} </li>
+          </ol>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
