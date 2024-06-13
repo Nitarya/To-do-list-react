345d52973fc421a35866c8cea88a5c3a42aa7a2b
Nitya <niittss27@gmail.com>
2024-06-13 15:30:27 +0530

    Placeolder added

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ function App() {
     <div className='main_div'>
       <div className='center_div'>
     <br/>
-    <h1></h1>
+    <h1>ToDo List</h1>
+    <br />
+    <input type="text" placeholder="Add a Items"/>
       </div>
 
     </div>
